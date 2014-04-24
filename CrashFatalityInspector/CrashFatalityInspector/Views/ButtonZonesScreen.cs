50f8d86dd9bf9c1fb59c8483db9c8edb4c2e39c9
Matthew Runde <mrunde76@gmail.com>
2014-04-23 21:21:01 -0500

    Final Project [95%-ish complete]

---
+++ CrashFatalityInspector/CrashFatalityInspector/Views/ButtonZonesScreen.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+//
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit;
+using Microsoft.Kinect.Toolkit.Controls;
+
+namespace CrashFatalityInspector.Views
+{
+    class ButtonZonesScreen
+    {
+        private Window mainWindow;
+        private KinectSensorChooser sensorChooser;
+        //
+        private Grid content;
+        private KinectRegion kRegion;
+        //
+        private KinectTileButton pacificButton;
+        private KinectTileButton mountainButton;
+        private KinectTileButton centralButton;
+        private KinectTileButton easternButton;
+
+        public ButtonZonesScreen(Window MainWindow, KinectSensorChooser SensorChooser)
+        {
+            // Get the main window and sensor objects
+            this.mainWindow = MainWindow;
+            this.sensorChooser = SensorChooser;
+            // Initialize display containers
+            CreateContentGrid();
+            CreateKinectRegion();
+            // Set up the display
+            this.kRegion.Content = this.content;
+            // Bind the Kinect sensor
+            var regionSensorBinding = new Binding("Kinect") { Source = SensorChooser };
+            BindingOperations.SetBinding(this.kRegion, KinectRegion.KinectSensorProperty, regionSensorBinding);
+        }
+
+        public void Show()
+        {
+            // Display this screen
+            this.mainWindow.Content = this.kRegion;
+        }
+
+        private void CreateContentGrid()
+        {
+            this.content = new Grid();
+            this.content.Name = Constants.ViewNames.TimeZoneScreen.ToString();
+            ImageBrush iBrush = new ImageBrush();
+            iBrush.ImageSource = new BitmapImage(new Uri(Constants.CFI_BUTTON_ZONE_IMAGE, UriKind.Relative));
+            this.content.Background = iBrush;
+            this.content.SizeChanged += ContentGridSizeChanged;
+            //
+            for (int i = 0; i < 4; i++)
+            {
+                ColumnDefinition cd = new ColumnDefinition();
+                cd.Width = new GridLength(this.mainWindow.ActualWidth / 4);
+                this.content.ColumnDefinitions.Add(cd);
+            }
+            //
+            this.pacificButton = InitializeKinectTileButton(Constants.TimeZones.Pacific);
+            this.mountainButton = InitializeKinectTileButton(Constants.TimeZones.Mountain);
+            this.centralButton = InitializeKinectTileButton(Constants.TimeZones.Central);
+            this.easternButton = InitializeKinectTileButton(Constants.TimeZones.Eastern);
+            //
+            Grid.SetColumn(this.pacificButton, 0);
+            Grid.SetColumn(this.mountainButton, 1);
+            Grid.SetColumn(this.centralButton, 2);
+            Grid.SetColumn(this.easternButton, 3);
+            //
+            this.content.Children.Add(this.pacificButton);
+            this.content.Children.Add(this.mountainButton);
+            this.content.Children.Add(this.centralButton);
+            this.content.Children.Add(this.easternButton);
+        }
+
+        void ContentGridSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            foreach (ColumnDefinition cd in this.content.ColumnDefinitions)
+            {
+                cd.Width = new GridLength(this.mainWindow.ActualWidth / 4);
+            }
+            foreach (KinectTileButton ktb in this.content.Children)
+            {
+                ktb.Width = this.mainWindow.ActualWidth / 4;
+                ktb.Height = this.mainWindow.ActualHeight;
+            }
+        }
+
+        private KinectTileButton InitializeKinectTileButton(Constants.TimeZones TimeZone)
+        {
+            KinectTileButton ktb = new KinectTileButton();
+            ktb.Name = TimeZone.ToString();
+            ktb.Height = this.mainWindow.ActualHeight;
+            ktb.Background = new LinearGradientBrush(Color.FromRgb(200, 200, 200), Color.FromRgb(77, 77, 77), 90);
+            ktb.Opacity = 0;
+            ktb.BorderThickness = new Thickness(0);
+            ktb.Padding = new Thickness(0);
+            ktb.Margin = new Thickness(0);
+            ktb.HorizontalAlignment = HorizontalAlignment.Center;
+            ktb.VerticalAlignment = VerticalAlignment.Center;
+            // TODO: We also need to add event handlers for the kinect sensor cursor...
+            // We only have the mouse events (for debugging) set up currently!
+            // Is this called the stylus, etc.?
+            ktb.MouseEnter += ButtonMouseEnter;
+            ktb.MouseLeave += ButtonMouseLeave;
+            ktb.Click += ZoneButtonClick;
+            return ktb;
+        }
+
+        void ButtonMouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            ((KinectTileButton)sender).Opacity = 0.25;
+        }
+
+        void ButtonMouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            ((KinectTileButton)sender).Opacity = 0;
+        }
+
+        void ZoneButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (((KinectTileButton)sender).Name == Constants.TimeZones.Pacific.ToString())
+            {
+#if (DEBUG)
+                Console.WriteLine("User clicked " + Constants.TimeZones.Pacific.ToString());
+#endif
+                StatesScreen ss = new StatesScreen(this.mainWindow, this.sensorChooser, Constants.TimeZones.Pacific);
+                ss.Show();
+            }
+            if (((KinectTileButton)sender).Name == Constants.TimeZones.Mountain.ToString())
+            {
+#if (DEBUG)
+                Console.WriteLine("User clicked " + Constants.TimeZones.Mountain.ToString());
+#endif
+                StatesScreen ss = new StatesScreen(this.mainWindow, this.sensorChooser, Constants.TimeZones.Mountain);
+                ss.Show();
+            }
+            if (((KinectTileButton)sender).Name == Constants.TimeZones.Central.ToString())
+            {
+#if (DEBUG)
+                Console.WriteLine("User clicked " + Constants.TimeZones.Central.ToString());
+#endif
+                StatesScreen ss = new StatesScreen(this.mainWindow, this.sensorChooser, Constants.TimeZones.Central);
+                ss.Show();
+            }
+            if (((KinectTileButton)sender).Name == Constants.TimeZones.Eastern.ToString())
+            {
+#if (DEBUG)
+                Console.WriteLine("User clicked " + Constants.TimeZones.Eastern.ToString());
+#endif
+                StatesScreen ss = new StatesScreen(this.mainWindow, this.sensorChooser, Constants.TimeZones.Eastern);
+                ss.Show();
+            }
+        }
+
+        private void CreateKinectRegion()
+        {
+            this.kRegion = new KinectRegion();
+        }
+    }
+}
---
+++ CrashFatalityInspector/CrashFatalityInspector/Views/ViewDataScreen.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+//
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit;
+using Microsoft.Kinect.Toolkit.Controls;
+
+namespace CrashFatalityInspector.Views
+{
+    class ViewDataScreen
+    {
+        private Window mainWindow;
+        private KinectSensorChooser sensorChooser;
+        //
+        private Dictionary<string, int> data;
+        //
+        private KinectRegion kRegion;
+        private Grid buttonGrid;
+        //
+        private double maxButtonWidth;
+        private double maxButtonHeight;
+        private double maxButtonDiameter;
+        //
+        private int largestDataValue;
+        private double multiplier;
+        //
+        List<DataButtonBubble> dataBubbles;
+
+        public ViewDataScreen(Window MainWindow, KinectSensorChooser SensorChooser, Utilities.State State, string VehicleType)
+        {
+            // Get the main window and sensor objects
+            this.mainWindow = MainWindow;
+            this.sensorChooser = SensorChooser;
+            // Get the desired data
+            this.data = Utilities.Data.SelectDataByStateAndVehicleType(State.Name, VehicleType);
+#if (DEBUG)
+            Utilities.Data.PrintDataByStateAndVehicleTypeToConsole(this.data, State.Name, VehicleType);
+#endif
+            // Initialize display containers
+            this.kRegion = CreateKinectRegion();
+            //
+            this.maxButtonWidth = this.mainWindow.Width / 7;
+            this.maxButtonHeight = this.mainWindow.Height / 2; // Also row separation point in Y
+            this.maxButtonDiameter = this.maxButtonHeight <= this.maxButtonWidth ? this.maxButtonHeight : this.maxButtonWidth;
+            //
+            this.largestDataValue = this.data.Max(value => value.Value);
+            this.multiplier = this.largestDataValue / this.maxButtonDiameter;
+#if (DEBUG)
+            Console.WriteLine("KRegion size: " + this.kRegion.Width + ", " + this.kRegion.Height);
+            Console.WriteLine("Max Button Width: " + this.maxButtonWidth.ToString());
+            Console.WriteLine("Max Button Height: " + this.maxButtonHeight.ToString());
+            Console.WriteLine("Max Button Diameter: " + this.maxButtonDiameter.ToString());
+            Console.WriteLine("Largest Data Value: " + this.largestDataValue.ToString());
+            Console.WriteLine("Data Multiplier: " + this.multiplier.ToString());
+#endif
+            //
+            this.dataBubbles = new List<DataButtonBubble>();
+            Random rand = new Random();
+            foreach (KeyValuePair<string, int> kvp in this.data)
+            {
+                KinectCircleButton kcb = new KinectCircleButton();
+                kcb.Width = kvp.Value / this.multiplier;
+                kcb.Height = kvp.Value / this.multiplier;
+                byte[] buffer = new byte[3];
+                rand.NextBytes(buffer);
+                SolidColorBrush scb = new SolidColorBrush();
+                scb.Color = Color.FromRgb(buffer[0], buffer[1], buffer[2]);
+                kcb.BorderBrush = scb;
+                kcb.Foreground = Brushes.White;
+                kcb.Content = kvp.Value.ToString();
+                kcb.Click += kcb_Click;
+                this.dataBubbles.Add(new DataButtonBubble(kcb, kvp.Value));
+            }
+            //
+            this.buttonGrid = new Grid();
+            this.buttonGrid.Width = this.kRegion.Width;
+            this.buttonGrid.Height = this.kRegion.Height;
+            this.buttonGrid.HorizontalAlignment = HorizontalAlignment.Center;
+            this.buttonGrid.VerticalAlignment = VerticalAlignment.Center;
+            this.buttonGrid.ShowGridLines = true;
+            for (int i = 0; i < 7; i++)
+            {
+                ColumnDefinition cd = new ColumnDefinition();
+                this.buttonGrid.ColumnDefinitions.Add(cd);
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                RowDefinition rd = new RowDefinition();
+                this.buttonGrid.RowDefinitions.Add(rd);
+            }
+            int bubblesPlaced = 0;
+            int row = 0;
+            foreach (DataButtonBubble db in this.dataBubbles)
+            {
+                Grid.SetColumn(db.Bubble, bubblesPlaced);
+                Grid.SetRow(db.Bubble, row);
+                bubblesPlaced++;
+                if (bubblesPlaced == 7)
+                {
+                    bubblesPlaced = 0;
+                    row = 1;
+                }
+                this.buttonGrid.Children.Add(db.Bubble);
+            }
+            this.kRegion.Content = this.buttonGrid;
+        }
+
+        void kcb_Click(object sender, RoutedEventArgs e)
+        {
+            TitleScreen ts = new TitleScreen(this.mainWindow, this.sensorChooser);
+            ts.Show();
+        }
+
+        public void Show()
+        {
+            // Display this screen
+            this.mainWindow.Content = this.kRegion;
+        }
+
+        private KinectRegion CreateKinectRegion()
+        {
+            KinectRegion kr = new KinectRegion();
+            kr.Width = this.mainWindow.ActualWidth - 50;
+            kr.Height = this.mainWindow.ActualHeight - 50;
+            kr.HorizontalAlignment = HorizontalAlignment.Center;
+            kr.VerticalAlignment = VerticalAlignment.Center;
+            return kr;
+        }
+    }
+
+    class DataButtonBubble
+    {
+        public KinectCircleButton Bubble { get; set; }
+        public int Data { get; set; }
+
+        public DataButtonBubble(KinectCircleButton Bubble, int Data)
+        {
+            this.Bubble = Bubble;
+            this.Data = Data;
+        }
+    }
+}
